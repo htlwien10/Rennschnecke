c0d79f3609700ac76645add5822a891c38d16d28
fkran <fkran@192.168.0.74>
2016-11-22 20:48:11 +0100

    Projekt angelegt

---
+++ Rennschnecke/src/rennschneck/rennen.java
@@ -0,0 +1,10 @@
+package rennschneck;
+
+public class rennen {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
---
+++ Rennschnecke/src/rennschneck/rennschnecke.java
@@ -0,0 +1,5 @@
+package rennschneck;
+
+class rennschnecke {
+
+}
