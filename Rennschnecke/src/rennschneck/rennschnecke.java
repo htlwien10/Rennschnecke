ccad53b7df7cb588415dfc9f0f717571ecc79775
fkran <fkran@FK_Schule>
2016-11-25 08:50:12 +0100

    Methode krieche hinzugefügt; vMax, range

--- Rennschnecke/src/rennschneck/rennschnecke.java
+++ Rennschnecke/src/rennschneck/rennschnecke.java
@@ -1,15 +1,30 @@
 package rennschneck;
 
+import java.util.Random;
+
 class rennschnecke {
-	
+	//Eigenschaften
 	private String name;
+	private double vMax;
+	private double range;
 	
+	
+	//Konstruktoren
 	public rennschnecke (String name) {
 		this.name = name;
+		this.vMax = 10;
+		this.range= 0;
 	}
 	
+	
+	//Methoden
 	public String getName() {
 		return name;
+	}
+	
+	public void krieche() {
+		Random zufall = new Random();
+		range = range + vMax * (zufall.nextInt(100)+1)/100;
 	}
 	
 }
